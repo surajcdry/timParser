eadf915fda40fd0f1e150b8f41ce1ef870dac13e
Suraj Chaudhary <isurajcdry@gmail.com>
2025-02-12 12:18:16 -0500

    Added custom number of posts to load
    Added an input prompt for the user to be able to choose how many posts to load. If the wanted posts not satisfied, go to the next page and so on.

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-source = requests.get('https://tim.blog/').text
-soup = BeautifulSoup(source, 'lxml')
+page = 0
 
 # opening/creating csv file to write the data
 csv_file = open('cms_scrape.csv', 'w')
@@ -11,27 +10,45 @@ csv_writer = csv.writer(csv_file)
 
 # creating heading row
 csv_writer.writerow(['date', 'headline', 'link to listen'])
+counter = 0
+wanted = int(input("Posts wanted: "))
 
+def parser():
+    global counter
+    global wanted
+    global page
 
-# grabbing single article one after another
-for article in soup.find_all('article'):
-    
-    headline = article.h3.text
-    date = article.find('time', class_='entry-date published').text
+    # grabbing single article one after another
+    source = requests.get(f'https://tim.blog/page/{page}').text
+    soup = BeautifulSoup(source, 'lxml')
+    for article in soup.find_all('article'):
+        
+        headline = article.h3.text
+        date = article.find('time', class_='entry-date published').text
 
-    # if podcast link not found (for example if simply a blog post), making sure the program doesn't break
-    try:
-        listen_link = article.find('a', class_='podcast-block__link')['href']
-    except:
-        listen_link = "Not Found"
+        # if podcast link not found (for example if simply a blog post), making sure the program doesn't break
+        try:
+            listen_link = article.find('a', class_='podcast-block__link')['href']
+        except:
+            listen_link = "Not Found"
 
-    # print parsed data in terminal
-    print("Date: " + date)
-    print("Title: " + headline)
-    print("Link to listen: " + listen_link)
-    print()
+        # print parsed data in terminal
+        print("Date: " + date)
+        print("Title: " + headline)
+        print("Link to listen: " + listen_link)
+        print()
 
-    # write parsed data to the cms_scrape.csv file
-    csv_writer.writerow([date, headline, listen_link])
+        counter += 1
+
+        # if wanted number of posts is reached, break the loop
+        if counter == wanted:
+            break
+
+        # write parsed data to the cms_scrape.csv file
+        csv_writer.writerow([date, headline, listen_link])
+
+while counter < wanted:
+    page +=1
+    parser()
 
 csv_file.close()
